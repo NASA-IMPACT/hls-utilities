e386fa7172ce1f7b4a40e63f7c16c24b048f5383
Sean Harkins <sharkinsgis@gmail.com>
2020-09-14 20:14:11 -0500

    Include element count tests for create_sr_hdf_xml.

--- tests/test_create_sr_hdf_xml.py
+++ tests/test_create_sr_hdf_xml.py
@@ -8,7 +8,7 @@ current_dir = os.path.dirname(__file__)
 test_dir = os.path.join(current_dir, "data")
 
 
-def test_create_sr_hdf_xml(tmpdir):
+def test_create_sr_hdf_xml_one(tmpdir):
     inputxml = os.path.join(test_dir,
                             "S2A_MSI_L1C_T17RKP_20200426_20200426.xml")
     # Unfortunate espa tmp prefixing only allows running in current dir.
@@ -23,6 +23,28 @@ def test_create_sr_hdf_xml(tmpdir):
         hdffile
     ], catch_exceptions=False)
     tree = lxml.etree.parse(outputxmlfile)
-    # pretty = lxml.etree.tostring(tree, encoding="unicode", pretty_print=True)
+    root = tree.getroot()
+    count = len(root[1])
+    assert count == 8
     assert result.exit_code == 0
 
+
+def test_create_sr_hdf_xml_two(tmpdir):
+    inputxml = os.path.join(test_dir,
+                            "S2A_MSI_L1C_T17RKP_20200426_20200426.xml")
+    # Unfortunate espa tmp prefixing only allows running in current dir.
+    os.chdir(tmpdir.strpath)
+    outputxmlfile = "outputxmlfile_1.xml"
+    hdffile = "two"
+
+    runner = CliRunner(echo_stdin=True)
+    result = runner.invoke(main, [
+        inputxml,
+        outputxmlfile,
+        hdffile
+    ], catch_exceptions=False)
+    tree = lxml.etree.parse(outputxmlfile)
+    root = tree.getroot()
+    count = len(root[1])
+    assert count == 6
+    assert result.exit_code == 0
