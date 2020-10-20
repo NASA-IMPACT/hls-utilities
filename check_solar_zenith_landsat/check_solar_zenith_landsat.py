1d07de4121354e6a8ef766cf7cdfd9cac65ee2a8
Sean Harkins <sharkinsgis@gmail.com>
2020-10-20 18:28:19 -0500

    Includ check_solar_zenith_landsat utility.

---
+++ check_solar_zenith_landsat/check_solar_zenith_landsat.py
@@ -0,0 +1,30 @@
+import click
+from check_solar_zenith_landsat.mtlutils import parsemeta
+
+
+@click.command()
+@click.argument(
+    "inputmtl",
+    type=click.Path(dir_okay=False, file_okay=True,),
+)
+def main(inputmtl):
+    """check_solar_zenith_landsat _MTL.txt"""
+    metadata = parsemeta(inputmtl)
+    try:
+        sun_elevation = float(
+            metadata["L1_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SUN_ELEVATION"]
+        )
+    except KeyError:
+        sun_elevation = float(
+            metadata["LANDSAT_METADATA_FILE"]["IMAGE_ATTRIBUTES"]["SUN_ELEVATION"]
+        )
+
+    solar_zenith = 90 - sun_elevation
+    if (solar_zenith > 76):
+        click.echo("invalid")
+    else:
+        click.echo("valid")
+
+
+if __name__ == "__main__":
+    main()
