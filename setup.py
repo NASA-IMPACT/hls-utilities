07f81a7e33a4f468883d3acb2e784844096d263b
Chris Holden <ceholden@gmail.com>
2026-01-12 12:23:57 -0500

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hls-utilities",
-    version="1.11.2",
+    version="1.11.3",
     packages=find_packages(),
     install_requires=[
         "click",
