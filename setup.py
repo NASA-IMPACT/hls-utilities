212d9194039abaca4983b6186e25f14dcd35441f
Chris Holden <ceholden@gmail.com>
2026-01-12 12:32:57 -0500

    fix: bump moto for python 3.12 ssl.wrap_socket removal

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     include_package_data=True,
     extras_require={
         "dev": ["ruff"],
-        "test": ["ruff", "pytest", "Jinja2==2.10.1", "moto[s3]~=2.0.8", "markupsafe==2"]
+        "test": ["ruff", "pytest", "Jinja2==2.10.1", "moto[s3]", "markupsafe==2"]
     },
     entry_points={"console_scripts": [
         "apply_s2_quality_mask=apply_s2_quality_mask.apply_s2_quality_mask:main",
